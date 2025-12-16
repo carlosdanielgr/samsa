763f6925c98334f2e148ae18de739228368d2510
carlosdanielgr <carlosguzman9812@gmail.com>
2025-12-15 21:37:09 -0500

    feat: cover video created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Cover from "./components/Cover/Cover";
 import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <Cover />
     </>
   );
 }
---
+++ src/components/Cover/Cover.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Cover.scss";
+
+const Cover: React.FC = () => {
+  return (
+    <section className="cover">
+      <video
+        // autoPlay
+        muted
+        loop
+        preload="auto"
+        playsInline
+        src="./src/assets/videos/Reel Portada_web.mp4"
+      ></video>
+    </section>
+  );
+};
+
+export default Cover;
